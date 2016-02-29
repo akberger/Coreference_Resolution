e3c95405020893fd06c0bd0ae398319b506a0e51
Adam_Berger <akberger@brandeis.edu>
2016-02-29 17:53:29 -0500

    set CR objects and working on pipeline

--- CoreferenceResolutionObjects.py
+++ CoreferenceResolutionObjects.py
@@ -11,7 +11,8 @@ from nltk.tree import *
 
 class Word(object):
 
-	def __init__(self, s, entity_type=None, is_entity=False):
+	def __init__(self, s, index, sent_num, entity_type=None, is_entity=False):
+		self.index = index
 		self.word = s[3]
 		self.POS = s[4]
 		self.POS_phrase = s[5]
@@ -20,6 +21,8 @@ class Word(object):
 		self.sense2 = s[8] if s[8] is not '_' else None
 		self.entity_type = None
 		self.is_entity = True if is_entity else False
+		self.chain = None
+		self.sent_num = sent_num
 		#add other features
 
 class Sentence(object):
@@ -28,18 +31,20 @@ class Sentence(object):
 	features of that word in the format of CONLL 2012 data.
 	"""
 
-	def __init__(self, word_strings):
+	def __init__(self, word_strings, sent_num):
 		#words - Word objects for each word in the sentence
-		#entities - list of tuples (entity string, [string start index, end index])
-		self.words, self.entities = self.words_from_string(word_strings) 		#list of Word objects
+		#entities - list of lists of Word objects. Each index corresponds to an entity
+		self.sent_num = sent_num
+		self.words, self.entities = self.words_from_string(word_strings) 
 		self.tree = self.make_tree(word_strings)
+		
 
 	def words_from_string(self, word_strings):
 		"""create Word objects from string representing a word and its features"""
 
 		words = []
 		on_entity = 0 # track how many entities are currently being processes
-		entities = defaultdict()
+		entities = []
 		current_entities = defaultdict(list)
 		entity_type = None
 
@@ -49,19 +54,26 @@ class Sentence(object):
 			on_entity += word[-1].count('(')
 			if '(' in word[10]:
 				entity_type = word[10][1:-1]
-			words.append(Word(word, entity_type=entity_type, is_entity=on_entity))
+			new_word = Word(word, i, self.sent_num, entity_type=entity_type, is_entity=on_entity)
+			words.append(new_word)
+			#words.append(Word(word, i, self.sent_num, entity_type=entity_type, is_entity=on_entity))
 			
 			# keep track of the entity strings in sentence and touple it with its
 			#location in the sentence for indexing later
 			if on_entity:
 				for j in xrange(on_entity):
 					# pair the word with its index in the sentence
-					pair = [word[3], i]
-					current_entities[j].append(tuple(pair))
+					#pair = [word[3], i]
+					#current_entities[j].append(tuple(pair))
+					current_entities[j].append(new_word.word)
 			else:
+				"""
 				for entity in current_entities:
 					string, indices = self.resolve_entity(current_entities[entity])
-					entities[string] = indices
+					entities.append({string : indices})
+				"""
+				for entity in current_entities:
+					entities.append(current_entities[entity])
 				current_entities = defaultdict(list)
 
 			# end entity and entity type if there is a closure
@@ -103,13 +115,90 @@ class Sentence(object):
 
 class Document(object):
 
-	def __init__(self, sentences):
-		self.sentences = sentences
-		self.entities = list()
-		self.chains = {}
+	def __init__(self, document, basename):
+		self.sentences = self.make_sentences(document)
+		#make a flat list of all entities in the document so each index is a 
+		#list corresponding to an entity
+		#self.entities = [j for j in i for i in [s.entities for s in self.sentences]]
+		self.entities = self.flatten_entities(self.sentences)
+		self.chains = defaultdict(list)
+		self.basename = basename
+
+	def make_sentences(self, f):
+		"""make a document from a CONLL file"""
+
+		document_sentences = []
+		sentence = []
+		sent_num = 0
+
+		for i, line in enumerate(f):
+			if not line.startswith('#'):
+				if line.split():
+					sentence.append(line)
+				else:
+					document_sentences.append(Sentence(sentence, sent_num))
+					sentence = []
+					sent_num += 1
+
+		return document_sentences
+
+	def flatten_entities(self, sentences):
+		entities = []
+		for s in sentences:
+			#print s.entities
+			for i in s.entities:
+				entities.append(i)
+
+		return entities
 
 
+def test():
+	instance = ['bc/cctv/00/cctv_0000   0    0         With     IN  (TOP(S(PP*           -    -   -   Speaker#1       *             *   (ARGM-MNR*        *      -',
+				'bc/cctv/00/cctv_0000   0    1        their   PRP$        (NP*           -    -   -   Speaker#1       *             *            *        *    (18)',
+				'bc/cctv/00/cctv_0000   0    2       unique     JJ           *           -    -   -   Speaker#1       *             *            *        *      -',
+				'bc/cctv/00/cctv_0000   0    3        charm     NN          *))       charm   -   -   Speaker#1       *             *            *)       *      -',
+				'bc/cctv/00/cctv_0000   0    4            ,      ,           *           -    -   -   Speaker#1       *             *            *        *      -',
+				'bc/cctv/00/cctv_0000   0    5        these     DT        (NP*           -    -   -   Speaker#1       *             *       (ARG0*        *    (18',
+				'bc/cctv/00/cctv_0000   0    6         well     RB      (ADJP*           -    -   -   Speaker#1       *    (ARGM-EXT*)           *        *      -',
+				'bc/cctv/00/cctv_0000   0    7           -    HYPH           *           -    -   -   Speaker#1       *             *            *        *      -',
+				'bc/cctv/00/cctv_0000   0    8        known    VBN           *)        know  01   2   Speaker#1       *           (V*)           *        *      -',
+				'bc/cctv/00/cctv_0000   0    9      cartoon     NN           *           -    -   -   Speaker#1       *        (ARG1*)           *        *      -',
+				'bc/cctv/00/cctv_0000   0   10       images    NNS           *)       image   -   2   Speaker#1       *      (C-ARG1*)           *)       *     18)',
+				'bc/cctv/00/cctv_0000   0   11         once     RB      (ADVP*           -    -   -   Speaker#1       *             *   (ARGM-TMP*        *      -',
+				'bc/cctv/00/cctv_0000   0   12        again     RB           *)          -    -   -   Speaker#1       *             *            *)       *      -',
+				'bc/cctv/00/cctv_0000   0   13       caused    VBD        (VP*        cause  01   1   Speaker#1       *             *          (V*)       *      -',
+				'bc/cctv/00/cctv_0000   0   14         Hong    NNP      (S(NP*           -    -   -   Speaker#1   (GPE*             *            *   (ARG1*    (23',
+				'bc/cctv/00/cctv_0000   0   15         Kong    NNP           *)          -    -   -   Speaker#1       *)            *            *        *)    23)',
+				'bc/cctv/00/cctv_0000   0   16           to     TO        (VP*           -    -   -   Speaker#1       *             *            *        *      -',
+				'bc/cctv/00/cctv_0000   0   17           be     VB        (VP*           be  01   1   Speaker#1       *             *            *      (V*)     -',
+				'bc/cctv/00/cctv_0000   0   18            a     DT     (NP(NP*           -    -   -   Speaker#1       *             *            *   (ARG2*      -',
+				'bc/cctv/00/cctv_0000   0   19        focus     NN           *)       focus   -   1   Speaker#1       *             *            *        *      -',
+				'bc/cctv/00/cctv_0000   0   20           of     IN        (PP*           -    -   -   Speaker#1       *             *            *        *      -',
+				'bc/cctv/00/cctv_0000   0   21    worldwide     JJ        (NP*           -    -   -   Speaker#1       *             *            *        *      -',
+				'bc/cctv/00/cctv_0000   0   22    attention     NN     *)))))))   attention   -   3   Speaker#1       *             *            *        *)     -',
+				'bc/cctv/00/cctv_0000   0   23            .      .          *))          -    -   -   Speaker#1       *             *            *        *      -',]
+	"""
+	d = Document(instance)
+	print d.entities
+	for s in d.sentences:
+		print s.entities
+		print s.tree
+	"""
+	S = Sentence(instance, 0)
+	print S.entities
+	print S.tree
+
+
+
+if __name__ == '__main__':
+	test()
+
+
+'''
 class Node(object):
+	"""
+	irrelevant because using NLTK for SyntaxTrees
+	"""
 
 	def __init__(self, POS, data="", parent=""):
 		self.POS = POS
@@ -200,41 +289,4 @@ class SyntaxTree(object):
 			node.parent = cur
 			if cur:
 				cur.add_child(node)
-
-def test():
-	instance = ['bc/cctv/00/cctv_0000   0    0         With     IN  (TOP(S(PP*           -    -   -   Speaker#1       *             *   (ARGM-MNR*        *      -',
-				'bc/cctv/00/cctv_0000   0    1        their   PRP$        (NP*           -    -   -   Speaker#1       *             *            *        *    (18)',
-				'bc/cctv/00/cctv_0000   0    2       unique     JJ           *           -    -   -   Speaker#1       *             *            *        *      -',
-				'bc/cctv/00/cctv_0000   0    3        charm     NN          *))       charm   -   -   Speaker#1       *             *            *)       *      -',
-				'bc/cctv/00/cctv_0000   0    4            ,      ,           *           -    -   -   Speaker#1       *             *            *        *      -',
-				'bc/cctv/00/cctv_0000   0    5        these     DT        (NP*           -    -   -   Speaker#1       *             *       (ARG0*        *    (18',
-				'bc/cctv/00/cctv_0000   0    6         well     RB      (ADJP*           -    -   -   Speaker#1       *    (ARGM-EXT*)           *        *      -',
-				'bc/cctv/00/cctv_0000   0    7           -    HYPH           *           -    -   -   Speaker#1       *             *            *        *      -',
-				'bc/cctv/00/cctv_0000   0    8        known    VBN           *)        know  01   2   Speaker#1       *           (V*)           *        *      -',
-				'bc/cctv/00/cctv_0000   0    9      cartoon     NN           *           -    -   -   Speaker#1       *        (ARG1*)           *        *      -',
-				'bc/cctv/00/cctv_0000   0   10       images    NNS           *)       image   -   2   Speaker#1       *      (C-ARG1*)           *)       *     18)',
-				'bc/cctv/00/cctv_0000   0   11         once     RB      (ADVP*           -    -   -   Speaker#1       *             *   (ARGM-TMP*        *      -',
-				'bc/cctv/00/cctv_0000   0   12        again     RB           *)          -    -   -   Speaker#1       *             *            *)       *      -',
-				'bc/cctv/00/cctv_0000   0   13       caused    VBD        (VP*        cause  01   1   Speaker#1       *             *          (V*)       *      -',
-				'bc/cctv/00/cctv_0000   0   14         Hong    NNP      (S(NP*           -    -   -   Speaker#1   (GPE*             *            *   (ARG1*    (23',
-				'bc/cctv/00/cctv_0000   0   15         Kong    NNP           *)          -    -   -   Speaker#1       *)            *            *        *)    23)',
-				'bc/cctv/00/cctv_0000   0   16           to     TO        (VP*           -    -   -   Speaker#1       *             *            *        *      -',
-				'bc/cctv/00/cctv_0000   0   17           be     VB        (VP*           be  01   1   Speaker#1       *             *            *      (V*)     -',
-				'bc/cctv/00/cctv_0000   0   18            a     DT     (NP(NP*           -    -   -   Speaker#1       *             *            *   (ARG2*      -',
-				'bc/cctv/00/cctv_0000   0   19        focus     NN           *)       focus   -   1   Speaker#1       *             *            *        *      -',
-				'bc/cctv/00/cctv_0000   0   20           of     IN        (PP*           -    -   -   Speaker#1       *             *            *        *      -',
-				'bc/cctv/00/cctv_0000   0   21    worldwide     JJ        (NP*           -    -   -   Speaker#1       *             *            *        *      -',
-				'bc/cctv/00/cctv_0000   0   22    attention     NN     *)))))))   attention   -   3   Speaker#1       *             *            *        *)     -',
-				'bc/cctv/00/cctv_0000   0   23            .      .          *))          -    -   -   Speaker#1       *             *            *        *      -',]
-
-	sentence = Sentence(instance)
-	print sentence.entities
-	print sentence.tree
-
-
-
-if __name__ == '__main__':
-	test()
-
-
-
+'''
--- Coreference_Resolution.py
+++ Coreference_Resolution.py
@@ -1,29 +1,131 @@
 import os
 import sys
-from CoreferenceResolutionObjects import Word, Sentence, Document, Node, SyntaxTree
+from CoreferenceResolutionObjects import Word, Sentence, Document
+from nltk.corpus import stopwords
+from collections import defaultdict
 
 class CoreferenceResolution(object):
 	def __init__(self):
 		self.documents = list()
+		self.stopwords = set(stopwords.words('english'))
 
-	def add_doc(self, f):
-		document = []
-		sentence = []
-		for i, line in enumerate(f):
-			if i > 0:
-				if line.split():
-					sentence.append(line)
-				else:
-					document.append(Sentence(sentence))
-					sentence = []
+	def add_doc(self, f, basename):
+		self.documents.append(Document(f, basename))
 
-#TODO: test sentence class
+	def write_output(self, outdir):
+		for i, doc in enumerate(self.documents):
+			with open(os.path.join(outdir, doc.basename)) as outfile:
+				##begin document (bc/cctv/00/cctv_0000); part 000
+				outfile.write("begin document (%s); part 000" % doc.basename)
+				for sent in doc.sentences:
+					for i, word in enumerate(sent):
+						chain = '-'
+						if word.chain and i > 0 and not sent[i-1].chain:
+							chain = '(' + word.chain
+						elif word.chain and i < len(sent) and not sent[i+1].chain:
+							chain = word.chain + ')'
+						elif (word.chain and i > 0 
+										 and i < len(sent) 
+										 and not sent[i-1].chain 
+										 and not sent[i+1].chain):
+							chain = '(' + word.chain + ')'
+
+						outfile.write('\t').join([doc.basename, chain])
+				outfile.write('#end document')
+
+
+
+	def chain(self, doc, e1, e2, chain=None):
+		"""make sure e1 and e2 are in the same chain in the document"""
+		if e1.chain and e2.chain:
+			e1_chain = e1[0].chain
+			for entity in doc.chains[e2[0].chain]:
+				doc.chains[e1_chain].append(entity)
+
+		else:
+			if doc.chains[chain]:
+				for e in [e1, e2]:
+					if e not in doc.chains[chain]:
+						doc.chains[chain].append(e)
+						for word in e: 
+							word.chain = chain
+
+
+	def string_match(self, doc, partial=False):
+		matches = defaultdict(list)
+		"""
+		compare every entity to every other entity store matches as mapping 
+		of their map number to a list each list index is the string and a 
+		tuple pointing to the location of the string in the document
+		{0 : ['string', (sent_num, [entity start index, end index])]}
+		"""
+		print doc.entities
+		for i, e1 in enumerate(doc.entities):
+			chained = False
+			e1_words = self.resolve_entity(e1)
+			for j, e2 in enumerate(doc.entities):
+				#make sure we don't compare more than we have to
+				#and we don't compare words already in a chain
+				if j > i and e1.chain is None and e2.chain is None: 
+						if e1_words == self.resolve_entity(e2):
+							self.chain(doc, e1, e2, chain=i)
+							chained = True
+			if not chained:
+				#initializie the rest of the clusters
+				self.chain(doc, e1, None, i)
+
+	def precise_constructs(self, doc, constructs=[',','is','was']):
+		for i, e1 in enumerate(doc.entities):
+			e1_acronym = [word.word[0] for word in e1]
+			for j, e2 in enumerate(doc.entities):
+				if j > i and e1[0].chain is not e2[0].chain:
+					# Check appositives and and Predicate Nominatives
+					if e1[0].sent_num == e2[0].sent_num:
+						if (e1[-1].index + 2 == e2[0].sent_num 
+						#the word between the two entities
+							and doc.sentences[e1[0].sent_num][e1[-1].index + 1] in constructs):
+							#combine the chains that the two entities are in
+							self.chain(doc, e1, e2)
+					# Check acronyms
+					if e1_acronym == [word.word[0] for word in e2]:
+						self.chain(doc, e1, e2)
+	"""
+	def partial_string_match(self, doc):
+		for i, e1 in enumerate(doc.entities):
+			e1_words = set([word.word for word in e1 if word not in self.stopwords])
+			for j, e2 in enumerate(doc.entities):
+				if j > i and e1[0].chain is not e2[0].chain: 
+					# Check word inclusion
+					if len(e1) > len(e2):
+						if e1_words.issuperset(set(word.word for word in e2 if word not in self.stopwords))
+	"""
+
+
+
+
+
+
+	def resolve_entity(self, e):
+		print e
+		return [word.word for word in e]
+
+	def resolve(self):
+		phases = [self.string_match, #must be run first
+				  self.precise_constructs
+				  #self.partial_string_match()
+				  ]
+
+		for phase in phases:
+			for document in self.documents:
+				phase(document)
 
 #TODO: write each phase in for seive method
+# TODO: reduce redundancy resolve phases. Maybe do both enumeration for loops
+# in resolve() and do the j > i and same chain check, then pass to the phase methods
 
 if __name__ == '__main__':
 	rootdir = sys.argv[1]
-	search_term = sys.argv[2]
+	search_term = 'gold_conll'
 
 	cr = CoreferenceResolution()
 
@@ -31,4 +133,30 @@ if __name__ == '__main__':
 		for fi in files:
 			if fi.endswith(search_term):
 				f = open(os.path.join(directory, fi))
-				cr.add_doc(f)
+				cr.add_doc(f, os.path.basename(fi))
+	"""
+	for doc in cr.documents:
+		for sentence in doc.sentences:
+			print [word.word for word in sentence]
+	"""
+
+	cr.resolve()
+	#cr.write_output(sys.argv[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
